19a370ab8e5a6dbb018e166522ad70a8580ac45e
htcvivek <vr27jul@gmail.com>
2013-04-07 02:02:35 +0530

    remove vimeo_project and readme file from pathsearch repo

--- vimeo_project/vimeouser/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-from models import Vimeouser
-
-class VimeouserAdmin(admin.ModelAdmin):
-    list_display = ('name','url','paying_user','staff_pick_video','video_upload')
-
-admin.site.register( Vimeouser, VimeouserAdmin)
