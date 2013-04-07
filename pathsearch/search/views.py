ad0165df9634dccb43f74cc89c1fc702f8233720
htcvivek <vr27jul@gmail.com>
2013-04-07 06:00:42 +0530

    change for highlighting

--- pathsearch/search/views.py
+++ pathsearch/search/views.py
@@ -9,9 +9,13 @@ def ajax_list(request):
     if request.method=='GET' and input_data:
         data=input_data.split()
         query_data='+'.join(['*%s*' % each for each in data])
-        url='%sPATH/select?q=%s&start=0&rows=300000&wt=json&indent=true&hl=true&hl.simple.pre=<em>&hl.simple.post=</em>'% (SOLR_URL,query_data)
+        url='%sPATH/select?q=%s&start=0&rows=300000&wt=json&indent=true&hl=true&hl.simple.pre=<em>&hl.simple.post=</em>&hl.q=%s&hl.fragsize=0'% (SOLR_URL,query_data,query_data)
         res=urllib2.urlopen(url).read()
         res=json.loads(res)
         total=res['response']['numFound']
         result=res['response']['docs']
+        i=0
+        while i<total:
+            result[i]['path']=res['highlighting'][result[i]['path']]['path'][0]
+            i+=1
         return render_to_response('ajax_list.html',locals())
